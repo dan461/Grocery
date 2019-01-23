d3eeb803fb574f56023fb96598340cde7fa9586b
Dan Shriver <dshriver53@gmail.com>
2019-01-23 15:55:02 -0600

    add properties to CartViewModel

--- Grocery/CartViewModel.swift
+++ Grocery/CartViewModel.swift
@@ -10,5 +10,23 @@ import Foundation
 
 class CartViewModel
 {
+    var selectedItem: CartItem?
+    var sharedInv = Inventory.shared
+    var invArray: [CartItem] = []
+    
+    var total: Double = 0.0
+    
+    var cartItems: [CartItem]
+    
+    init()
+    {
+        cartItems = [CartItem]()
+    }
+    
+    public func createInventory()
+    {
+        invArray = [sharedInv.soup, sharedInv.bread, sharedInv.oatmeal, sharedInv.apples, sharedInv.bananas, sharedInv.steak, sharedInv.chicken]
+    }
+    
     
 }
